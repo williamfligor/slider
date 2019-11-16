8202b11e1f5c5d5cc2bdd8dcbdc2253001d205f3
William Fligor <williamfligor@Williams-MacBook-Pro-2.local>
2019-11-16 11:25:28 -0500

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,58 @@
+import * as express from 'express';
+import * as mustacheExpress from 'mustache-express';
+import * as fileUpload from 'express-fileupload';
+import * as asyncHandler from 'express-async-handler';
+import * as convert from './convert';
+
+const SLIDES_DIR = __dirname + '/../data/slides';
+
+const app = express();
+const port = 3000;
+
+app.engine('html', mustacheExpress());
+app.use(fileUpload());
+
+app.use(express.static(convert.SLIDES_DIR))
+app.set('view engine', 'html');
+app.set('views', __dirname + '/../views');
+
+app.get('/upload', (req, res) => {
+    res.render('upload.html')
+});
+
+app.post('/upload', asyncHandler(async (req, res) => {
+    if (!req.files || Object.keys(req.files).length === 0) {
+        res.status(400).send('No files were uploaded.');
+        return;
+    }
+
+    let file = req.files.presentation;
+
+    try {
+        await convert.pdfToImages(file);
+
+        res.send('Uploaded!')
+    } catch (e) {
+        console.error('Error processing upload: ', e)
+        res.status(500).send('Error processing upload...');
+    }
+}));
+
+app.get('/', asyncHandler(async (req, res) => {
+    const files = await convert.listFiles(convert.SLIDES_DIR);
+
+    const presentationImages : string[] = [];
+
+    for (const f of files) {
+        if (f.endsWith('.jpg')) {
+            presentationImages.push(f);
+        }
+    }
+
+    res.render('presentation.html', {
+        images: presentationImages,
+        slideInterval: 1000 * 1 * 1
+    })
+}));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
